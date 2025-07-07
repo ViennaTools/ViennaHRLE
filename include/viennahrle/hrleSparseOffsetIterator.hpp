ad77b516cfa8fcbd89a2b02ada840a7f567b0bb6
reiter <t.reiter1@live.de>
2025-07-07 16:16:19 +0200

    Bump version

--- include/viennahrle/hrleSparseOffsetIterator.hpp
+++ include/viennahrle/hrleSparseOffsetIterator.hpp
@@ -601,8 +601,6 @@ public:
     std::cout << "move_inverse: " << move_inverse << std::endl;
   }
 
-  SparseOffsetIterator() = default;
-
   template <class V>
   SparseOffsetIterator(hrleDomain &passedDomain, const V &o,
                        bool reverse = false)
